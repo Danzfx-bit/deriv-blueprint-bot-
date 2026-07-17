4f58dafe386a195116e7368deb63ba8b3ff3792d
Danzfx-bit <sibandadanzel@gmail.com>
2026-07-17 11:46:39 +0200

    Update get_latest_tick method to handle tick responses
    Refactor get_latest_tick to continuously receive ticks until a valid response is received.

--- deriv_client.py
+++ deriv_client.py
@@ -1,22 +1,22 @@
 import json
 import websocket
 
-
 class DerivClient:
 
     def __init__(self, app_id):
         self.url = f"wss://ws.derivws.com/websockets/v3?app_id={app_id}"
 
     def get_latest_tick(self, symbol):
-
         ws = websocket.create_connection(self.url)
 
         ws.send(json.dumps({
-            "ticks": symbol
+            "ticks": symbol,
+            "subscribe": 1
         }))
 
-        response = json.loads(ws.recv())
+        while True:
+            response = json.loads(ws.recv())
 
-        ws.close()
-
-        return response
+            if "tick" in response:
+                ws.close()
+                return response
