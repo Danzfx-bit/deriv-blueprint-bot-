5948902304237367ba3a15f840498f137d9b41d7
Danzfx-bit <sibandadanzel@gmail.com>
2026-07-17 04:10:09 +0200

    Add application configuration constants

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
+APP_NAME = "deriv blueprint bot"
 
+MARKETS = [
+    "R_10",
+    "R_10_1S",
+    "R_25",
+    "R_25_1S",
+    "R_50",
+    "R_50_1S",
+    "R_75",
+    "R_75_1S",
+    "R_100",
+    "R_100_1S"
+]
+
+TIMEFRAMES = [
+    "Ticks",
+    "1 Minute",
+    "5 Minutes",
+    "15 Minutes",
+    "30 Minutes",
+    "1 Hour"
+]
